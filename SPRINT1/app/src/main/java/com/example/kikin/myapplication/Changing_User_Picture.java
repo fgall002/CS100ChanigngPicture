0a2336fcd4e19c25e1b6ccc43a1dafe05fd131c7
Francisco Gallegols <fgall002@ucr.edu>
2018-05-01 17:12:01 -0700

    First Commit

---
+++ SPRINT1/app/src/main/java/com/example/kikin/myapplication/Changing_User_Picture.java
@@ -0,0 +1,92 @@
+package com.example.kikin.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class Changing_User_Picture extends AppCompatActivity implements View.OnClickListener {
+    Uri getImageUrl;
+    private static final String TAG = "Updated";
+    private static final int RESULT_LOAD_IMAGE = 1;
+    ImageView imageToUpload, downloadImage;
+    Button bUploadImage, bDownloadImage,bConfirmImage;
+    EditText uploadImageName, downloadImageName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changing__user__picture);
+
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+//        downloadImage = (ImageView) findViewById(R.id.downloadImage);
+
+        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+        bConfirmImage= (Button) findViewById(R.id.bConfirmImage);
+
+//        bDownloadImage = (Button) findViewById(R.id.bDownloadImage);
+
+        //    uploadImageName = (EditText) findViewById(R.id.etUploadName);
+//        downloadImageName = (EditText) findViewById(R.id.etDownloadName);
+
+        imageToUpload.setOnClickListener(this);
+        bUploadImage.setOnClickListener(this);
+//        bDownloadImage.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.imageToUpload:
+//                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
+                break;
+            case R.id.bUploadImage:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
+                break;
+            case R.id.bConfirmImage:
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if(user != null) {
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                            .setPhotoUri(getImageUrl)
+                            .build();
+                    user.updateProfile(profileUpdates)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d(TAG, "User profile update.");
+                                    }
+                                }
+                            });
+
+                }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data !=null){
+            Uri selectedImage= data.getData();
+            getImageUrl = data.getData();
+            imageToUpload.setImageURI(selectedImage);
+
+
+        }
+    }
+}
